5e3ec341f7bb1ca986821fe777ba18d0d9530fa3
kristiyan <kristiyan.c@gmail.com>
2019-02-05 19:16:33 +0000

    Final fixes

--- src/layout/AppHeader.vue
+++ src/layout/AppHeader.vue
@@ -32,14 +32,12 @@
 </template>
 <script>
 import BaseNav from "../components/BaseNav";
-import BaseDropdown from "../components/BaseDropdown";
 import CloseButton from "../components/CloseButton";
 
 export default {
   components: {
     BaseNav,
-    CloseButton,
-    BaseDropdown
+    CloseButton
   }
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import Vue from 'vue/dist/vue.min.js'
-import App from '../src/App.vue'
+import App from './App.vue'
 import VueRouter from 'vue-router'
 import Vuex from "vuex";
 import router from "../src/router";
@@ -12,17 +12,28 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 import { faCaretLeft, faCaretRight } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
+library.add({ faCaretLeft, faCaretRight });
+
+Vue.component('font-awesome-icon', FontAwesomeIcon);
+Vue.use(VueRouter);
+Vue.use(Vuex);
+Vue.use(Argon);
+Vue.use(VueAxios, axios);
+Vue.use(require('vue-cookies'));
+
 Vue.config.productionTip = false
 Vue.use(VueAxios, {
   secured: securedAxiosInstance,
   plain: plainAxiosInstance
 })
 
-new Vue({
-  router,
-  securedAxiosInstance,
-  plainAxiosInstance,
-  components: { App },
-  template: '<App/>'
-}).$mount("#app");
-
+document.addEventListener('DOMContentLoaded', () => {
+  new Vue({
+    store,
+    router,
+    securedAxiosInstance,
+    plainAxiosInstance,
+    components: { App },
+    template: '<App/>'
+  }).$mount("#app");
+})
