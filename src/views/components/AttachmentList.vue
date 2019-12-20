52d17b9037c003a7cc4cf31f95257c342f9d84ab
kyle <cookekyle97@gmail.com>
2019-12-20 12:08:22 +0000

    add file uploads to useful resources

---
+++ src/views/components/AttachmentList.vue
@@ -0,0 +1,96 @@
+<template>
+  <div>
+    <ul>
+      <div v-if="edit_mode">
+        <li v-for="file in file_meta">
+      <span v-if="file.file" :key="re_render">
+        <s v-if="file.deleted">{{ file.file.file.name }}</s>
+        <span v-else-if="file.new">{{ file.file.name }}</span>
+        <span v-else="file.new">{{ file.file.file.name }}</span>
+        <base-button type="danger" @click="file.deleted=!file.deleted; re_render+=1">Delete</base-button>
+      </span>
+          <span v-else>
+        New File: <input type="file" @change="processFile($event, file)">
+      </span>
+        </li>
+      </div>
+      <div v-else>
+        <li v-for="file in file_meta">
+          <a v-bind:href="file.file.file.path">{{ file.file.file.name }}</a>
+        </li>
+      </div>
+    </ul>
+    <base-button v-if="edit_mode" type="success" @click="add_file">Add file</base-button>
+  </div>
+</template>
+<script>
+  import BaseButton from "../../components/BaseButton";
+
+  export default {
+    name: "attachment_list",
+    components: {
+      BaseButton
+    },
+    props: {
+      attachments: {
+        type: Array,
+        description: "attachments to display"
+      },
+      edit_mode: {
+        type: Boolean
+      }
+    },
+    mounted: function () {
+    },
+    data: function () {
+      let new_meta = [];
+      this.attachments.forEach(x => new_meta.push({file: x}));
+      return {
+        file_meta: new_meta,
+        re_render: 0
+      }
+    },
+    methods: {
+      sync: function (resource_id) {
+        console.log("syncing resources");
+        let that = this;
+        //sends all files that are changed or new
+        this.file_meta.forEach(function (x) {
+          if (x.new && !x.deleted){
+            var formData = new FormData();
+            formData.append("document", x.file);
+            formData.append("useful_resource_id", resource_id);
+            console.log(formData);
+            that.axios.post('/api/useful_resource_attachments',
+              formData,
+              {
+              headers: {
+                'Content-Type': null,
+              }
+            }).then(function (response) {
+              x.id = response.data.id;
+              x.new = false;
+            })
+          } else if (x.deleted) {
+            that.axios.delete("/api/useful_resource_attachments/" + x.file.id).then(function (response) {
+              let index = that.file_meta.indexOf(x);
+              if (index > -1) {
+                that.file_meta.splice(index, 1);
+              }
+            })
+          }
+        })
+      },
+      add_file: function () {
+        console.log("File add clicked");
+        this.file_meta.push({file: null, new: true});
+      },
+      processFile(event, file) {
+        console.log(event.target.files);
+        file.file = event.target.files[0]
+      }
+    }
+  };
+</script>
+<style>
+</style>
--- src/views/components/UsefulResource.vue
+++ src/views/components/UsefulResource.vue
@@ -8,29 +8,35 @@
                 v-model="edit_resource_body">
         </textarea>
         <base-checkbox v-model="resource.restricted">Tutor Only?</base-checkbox>
+        <attachment_list ref="attachments" v-bind:attachments="resource.useful_resource_attachments" v-bind:edit_mode="edit_mode"/>
+        <br/>
         <base-button type="secondary" @click="edit_mode=false">Cancel</base-button>
-        <base-button type="primary" @click="$emit('update-resource', resource, edit_resource_body); edit_mode=false">Update</base-button>
+        <base-button type="primary" @click="$emit('update-resource', resource, edit_resource_body); edit_mode=false; sync_attachments()">Update</base-button>
         <a href="https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet" target="_blank">Markdown Cheatsheet</a>
       </div>
       <div v-else>
         <vue-markdown v-bind:source="resource.body" v-bind:html="false"></vue-markdown>
         <base-checkbox v-model="resource.restricted" disabled>Tutor Only</base-checkbox>
-        <base-button type="info" @click="edit_mode=true; edit_resource_body= this.resource.body">Edit</base-button>
+        <attachment_list ref="attachments" v-bind:attachments="resource.useful_resource_attachments" v-bind:edit_mode="edit_mode"/>
+        <base-button type="info" @click="edit_mode=true; edit_resource_body= resource.body">Edit</base-button>
         <base-button type="danger" @click="$emit('delete-resource', resource)">Delete</base-button>
       </div>
     </div>
     <div v-else>
       <vue-markdown v-bind:source="resource.body" v-bind:html="false"></vue-markdown>
+      <attachment_list ref="attachments" v-bind:attachments="resource.useful_resource_attachments" v-bind:edit_mode="edit_mode"/>
     </div>
   </div>
 </template>
 <script>
   import VueMarkdown from "vue-markdown/src/VueMarkdown";
   import BaseButton from "../../components/BaseButton";
+  import Attachment_list from "./AttachmentList";
 
   export default {
     name: "useful_resource",
     components: {
+      Attachment_list,
       VueMarkdown,
       BaseButton
     },
@@ -49,7 +55,9 @@
       }
     },
     methods: {
-
+      sync_attachments: function(){
+        this.$refs.attachments.sync(this.resource.id);
+      }
     }
   };
 </script>
